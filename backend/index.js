4f723046884c4cdcd833252cf61c05c80e24efc8
Arjun Kumanan <arjunkumanan123@gmail.com>
2025-03-01 01:30:52 +0800

    Add get user API

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
+const { authenticateToken } = require("./authentication");
+
 const client = new Client({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
@@ -46,12 +48,38 @@ app.post("/login", async (req, res) => {
 
         const user = result.rows[0];
         const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: "1h" });
-        res.json({ message: "Login successful", token });
+        return res.json({ message: "Login successful", user: { username: user.username, password: user.password_hash }, token });
     } catch (err) {
         console.error("Error executing query", err);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+// Get user information
+app.get("/get-user", authenticateToken, async (req, res) => {
+    try {
+        const { username } = req.user;
+        const query = `
+            SELECT first_name, last_name, date_of_birth, address, gender, nric
+            FROM users
+            WHERE username = $1`;
+        
+        const result = await client.query(query, [username]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const user = result.rows[0];
+        res.json({
+            message: "User information retrieved successfully",
+            user,
+        });
+    } catch (err) {
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
+
 
 app.listen(8000);
 module.exports = app;
